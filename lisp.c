72e77141c5001751e82037584f9131ef3c4bfa15
Kjell Post <kjell@irstafoto.se>
2022-01-29 14:22:04 +0100

    Changed to 3-bit tags

--- lisp.c
+++ lisp.c
@@ -4,33 +4,30 @@
 
 #define NL printf("\n");
 
+void error(char *s) {
+  fprintf(stderr, "%s\n", s);
+  exit(1);
+}
+
 // Objects are tagged 32-bit values
 
 typedef uint32_t Obj;
 
-// Objects are tagged with 2 most significant bits
-// number      00bbbbbb ... bbbbbbbb
-// symbol        01bbbbbb ... bbbbbbbb
-// pair        10bbbbbb ... bbbbbbbb
-// procedure   11bbbbbb ... bbbbbbbb
+// Objects are tagged with 3 most significant bits
+// number      000bbbbbb ... bbbbbbbb
+// symbol      001bbbbbb ... bbbbbbbb
+// pair        010bbbbbb ... bbbbbbbb
+// procedure   011bbbbbb ... bbbbbbbb
+// primitive   100bbbbbb ... bbbbbbbb
+// string      101
+// float       110
+// array       111
 
-#define PAIR_TAG 0
+#define PAIR_TAG   0
 #define SYMBOL_TAG 1
-#define NUM_TAG  2
-#define PROC_TAG 3
-
-Obj mknum(int n) {
-  return n | (NUM_TAG << 30);
-}
-
-Obj mksym(char *id) {
-  int n = lookup(id);
-  return n | (SYMBOL_TAG << 30);
-}
-
-Obj mkproc(int n) {
-  return n | (PROC_TAG << 30);
-}
+#define NUM_TAG    2
+#define PROC_TAG   3
+#define PRIM_TAG   4
 
 int objval(Obj n) {
   return n & (0x3fff);
@@ -49,19 +46,36 @@ Obj free_index = 1;		       // can't start at 0 because 0 means NIL
 Obj car(Obj expr) {  return thecars[expr]; }
 Obj cdr(Obj expr) {  return thecdrs[expr]; }
 
-Obj cons(Obj car, Obj cdr) {
-  if (free_index > MEMSIZE) {
-      printf("Error: memory full!\n");
-      return 0;
-  }
-      
-  thecars[free_index] = car;
-  thecdrs[free_index] = cdr;
-  return free_index++;
+Obj mknum(int n) {
+  return n | (NUM_TAG << 30);
 }
 
+Obj mksym(char *id) {
+  int n = lookup(id);
+  return n | (SYMBOL_TAG << 30);
+}
 
-Obj NIL_SYM, TRUE_SYM, IF_SYM, EQ_SYM, LET_SYM, ADD_SYM, SUB_SYM,
+Obj cons(Obj car, Obj cdr) {	/* aka mkpair */
+  if (free_index > MEMSIZE)
+      error("Error: memory full!");
+  thecars[free_index] = car;
+  thecdrs[free_index] = cdr;
+  return free_index++ | (PAIR_TAG << 30);
+}
+
+Obj mkproc(Obj parameters, Obj body, Obj env) {
+  /* a procedure is a triple (parameters body env) */
+  Obj p = cons(parameters, cons(body, env));
+  return objval(p) | (PROC_TAG << 30);
+}
+
+// an environment BINDING id -> value is represented simply as cons(id, value)
+// an ENVIRONMENT is a list of bindings ( ( id . value ) ( id . value ) .... )
+
+
+
+
+Obj NIL, TRUE_SYM, IF_SYM, EQ_SYM, LET_SYM, ADD_SYM, SUB_SYM,
   MUL_SYM, DIV_SYM, DEFINE_SYM, CAR_SYM, CDR_SYM, CONS_SYM, ATOM_SYM,
   QUOTE_SYM, LETREC_SYM, LAMBDA_SYM;
 
@@ -111,9 +125,21 @@ Obj pop() {
   return Stack[--StackPtr];
 }
 
+void prim_car() {
+  val = car(expr);
+}
+
+void (*primitives[])() = { prim_car };
+
 
 void init() {
-  NIL_SYM = mksym("NIL"); TRUE_SYM = mksym("TRUE"); IF_SYM = mksym("IF");
+
+  // primitives
+  
+
+
+
+  NIL = mksym("NIL"); TRUE_SYM = mksym("TRUE"); IF_SYM = mksym("IF");
   EQ_SYM = mksym("EQ");   LET_SYM = mksym("LET"); ADD_SYM = mksym("ADD");
   SUB_SYM = mksym("SUB"); MUL_SYM = mksym("MUL"); DIV_SYM = mksym("DIV");
   CAR_SYM = mksym("CAR"); CDR_SYM = mksym("CDR");
@@ -121,11 +147,12 @@ void init() {
   QUOTE_SYM = mksym("QUOTE"); LETREC_SYM = mksym("LETREC");
   DEFINE_SYM = mksym("DEFINE"); LAMBDA_SYM = mksym("LAMBDA");
   cont = EVAL_DISPATCH;
-  val = NIL_SYM;
-  unev = NIL_SYM;
-  argl = NIL_SYM;
-  proc = NIL_SYM;
+  val = NIL;
+  unev = NIL;
+  argl = NIL;
+  proc = NIL;
   StackPtr = 0;
+  
 }
 
 void display(Obj expr);
@@ -173,7 +200,7 @@ int is_assignment()      { return 0; }
 int is_definition()      { return DEFINE_SYM == car(expr); }
 int is_lambda()          { return 0; }
 int is_begin()           { return 0; }
-int is_application()     { return 0; }
+int is_application()     { return PAIR_TAG == objtype(expr); }
 
 
 void eval_dispatch() {
@@ -183,6 +210,9 @@ void eval_dispatch() {
     //    printf("cont = %d\n", cont);
     //    printf("expr = "); display(expr); NL;
     switch (cont) {
+    case EV_APPL_DID_OPERATOR:
+      // ...
+      break;
     case EVAL_DISPATCH:
       cont = PRINT_RESULT;
       if (is_self_evaluating()) {
@@ -213,7 +243,13 @@ void eval_dispatch() {
       } else if (is_begin()) {
 	cont = EV_BEGIN;
       } else if (is_application()) {
-	cont = EV_APPLICATION;
+	push(cont);
+	push(env);
+	unev = cdr(expr);
+	push(unev);
+	expr = car(expr);
+	cont = EV_APPL_DID_OPERATOR;
+	continue;
       } else
 	cont = UNKNOWN_EXPRESSION_TYPE;
       break;
@@ -230,12 +266,64 @@ void eval_dispatch() {
   }
 }
 
+void dumpval(Obj n) {
+  switch (objtype(n)) {
+  case PAIR_TAG:
+    if (objval(n))
+      printf("P%d ", objval(n));
+    else
+      printf("NIL");
+    break;
+  case NUM_TAG:
+    printf("INT %d ", objval(n));
+    break;
+  case SYMBOL_TAG:
+    printf("A%d ", objval(n));
+    break;
+  case PROC_TAG:
+    printf("$%d ", objval(n));
+    break;
+  }
+}
+
+void dump() {
+  int i, last;
+  for (last = MEMSIZE; last >= 0; last--)
+    if (thecars[last])
+      break;
+  
+  printf("      ");
+  for (i = 1; i <= last; i++) {
+    printf("%d:\t", i);
+  }
+  printf("\n");
+  printf("CARS: ");
+  for (i = 1; i <= last; i++) {
+    dumpval(thecars[i]);
+    printf("\t");
+  }
+  printf("\nCDRS: ");
+  for (i = 1; i <= last; i++) {
+    dumpval(thecdrs[i]);
+    printf("\t");
+  }
+  printf("\n");
+}
+
+
+
 int main() {
   // dump();
   //  testhashtab();
-  // dumphashtab();
   expr = mknum(42);
   eval_dispatch();
   expr = cons(QUOTE_SYM, cons(mknum(17), 0));
   eval_dispatch();
+  Obj p = mkproc(mksym("x"), cons(mksym("x"), NIL), 99);
+  printf("expr = %d %d\n", objval(p), objtype(p));
+  expr = cons(p, NIL);
+  
+  dump();
+  dumphashtab();
+
 }
