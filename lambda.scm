2290f1cf62ce9d00a986f917186d1331b955f18e
Kjell Post <kjell@irstafoto.se>
2022-02-07 22:16:30 +0000

    Test file

---
+++ lambda.scm
@@ -0,0 +1,37 @@
+(define id (lambda (x) x))
+(id 42)
+(define k (lambda (x y) x))
+(k 1 2)
+(define s (lambda (x y z) (x z (y z))))
+(s k k 17)
+
+42
+(quote 17)
+(quote (1 2 3))
+x
+(set! x 22)
+(define l123 (quote (1 2 3)))
+#t
+#f
+(if #t (quote a) (quote b))
+(if #f (quote a) (quote b))
+(if 42 (quote a) (quote b))
+(car (quote (1 2 3)))
+(cdr (quote (1 2 3)))
+((if #f + *) 2 3)
+(begin (define u 42) (set! u 99) u)
+
+;;; zero? returns #t if x is zero, #f otherwise
+(define zero? (lambda (x) (= x 0)))
+
+;;; recursive factorial function
+;;; this lisp interpreter can compute up to 12!
+(define fact
+  (lambda (n)
+    (if (zero? n)
+	1
+	(* n (fact (- n 1))))))
+
+(fact 12)
+
+;;; EOF
