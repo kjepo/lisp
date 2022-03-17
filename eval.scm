331cc9977d06633b43864368cab4232137d867c7
Kjell Post <kjell@irstafoto.se>
2022-03-17 13:35:51 +0100

    Added macros

--- eval.scm
+++ eval.scm
@@ -3,7 +3,7 @@
 
 (define eval*
   (lambda (exp env)
-    (cond ((self-evaluating? exp)       ; 42, "foobar"
+    (cond ((self-evaluating? exp)       ; 42, "foobar", #f
            exp)
           ((symbol? exp)                ; x, y, z
            (lookup exp env))
@@ -25,6 +25,8 @@
             (cadr exp)                  ; parameters
             (cddr exp)                  ; body
             env))
+          ((macro? exp)                 ; macro
+           (eval* (macro-expand exp) env))
           ((pair? exp)                  ; (f exp1 exp2 ... )
            (apply* (eval* (car exp) env)
                    (list-of-values (cdr exp) env)))
@@ -62,7 +64,7 @@
 
 (define self-evaluating?
   (lambda (exp)
-    (or (number? exp) (string? exp))))
+    (or (number? exp) (string? exp) (boolean? exp))))
 
 (define make-procedure
   (lambda (parameters body env)
@@ -97,7 +99,50 @@
 (define env0                            ; initial environment
   (list (cons 'car (cons 'primitive car))
         (cons 'cdr (cons 'primitive cdr))
-        (cons 'cons (cons 'primitive cons))))
+        (cons 'cons (cons 'primitive cons))
+        (cons 'eq? (cons 'primitive eq?))
+        (cons '+ (cons 'primitive +))
+        (cons '- (cons 'primitive -))
+        (cons '> (cons 'primitive >))
+        (cons '< (cons 'primitive <))))
+
+;;; (or e1 e2 ...) => (if e1 #t (or e2 ...))
+(define macro-or
+  (lambda (operands)
+    (if (null? operands) #f
+        (list 'if (car operands) #t (macro-or (cdr operands))))))
+
+;;; (and e1 e2 ... ) => (if e1 (and e2 ...) #f)
+(define macro-and
+  (lambda (operands)
+    (if (null? operands) #t
+        (list 'if (car operands) (macro-and (cdr operands)) #f))))
+
+;;; (cond ((t1 e1) (t2 e2) ...)) => (if t1 e1 (cond ((t2 e2) ...)))
+(define macro-cond
+  (lambda (body)
+    (if (null? body) '()
+        (append
+         (list 'if (caar body) (cadar body))
+         (list (macro-cond (cdr body)))))))
+
+(define macros                          ; initial macro definitions
+  (list (cons 'or macro-or)
+        (cons 'and macro-and)
+        (cons 'cond macro-cond)))
+
+(define macro?
+  (lambda (exp)
+    (and
+     (pair? exp)
+     (assoc (car exp) macros))))
+
+(define macro-expand
+  (lambda (macro)
+    (let ((m (assoc (car macro) macros)))
+      (if m
+          ((cdr m) (cdr macro))
+          (error "No macro expansion for " macro)))))
 
 (eval* '3 env0)
 (eval* 'foo '((foo . 17)))
@@ -110,3 +155,10 @@
           (define k (lambda (x) (lambda (y) x)))
           (define s (lambda (x) (lambda (y) (lambda (z) ((x z) (y z))))))
           (((s k) k) 123)) env0)
+(eval* '(or (eq? 3 0) (eq? 1 2) (eq? 5 5)) env0)
+(eval* '(and (eq? 3 3) (eq? 2 2)) env0)
+(eval* '(and (eq? 3 3) (eq? 2 2) (eq? 5 4)) env0)
+(eval* '(begin
+          (define zero? (lambda (x) (cond ((eq? x 0) #t) (#t #f))))
+          (define abs (lambda (x) (cond ((< x 0) (- 0 x)) ((> x 0) x) (#t 0))))
+          (abs -4)) env0)
