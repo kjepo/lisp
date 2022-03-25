ce39826668ab3ffa24908861136318d63fd71beb
Kjell Post <kjell@irstafoto.se>
2022-03-25 12:45:21 +0100

    Now using macros for cond/and, and define without lambda

--- differentiate.scm
+++ differentiate.scm
@@ -5,34 +5,26 @@
   (symbol? x))
 
 (define (same-variable? v1 v2)
-  (if (and (variable? v1) (variable? v2))
-	    (eq? v1 v2)
-	    #f))
+  (and (variable? v1) (variable? v2) (eq? v1 v2)))
 
 (define (=number? exp num)
-  (if (number? exp)
-	    (eq? exp num)
-	    #f))
+  (and (number? exp) (eq? exp num)))
 
 (define (make-sum a1 a2)
   (cond ((=number? a1 0) a2)
         ((=number? a2 0) a1)
-        ((and (number? a1) (number? a2))
-         (+ a1 a2))
+        ((and (number? a1) (number? a2)) (+ a1 a2))
         (#t (list '+ a1 a2))))
 
 (define (make-product m1 m2)
   (cond ((or (=number? m1 0) (=number? m2 0)) 0)
         ((=number? m1 1) m2)
         ((=number? m2 1) m1)
-        ((and (number? m1) (number? m2))
-         (* m1 m2))
+        ((and (number? m1) (number? m2)) (* m1 m2))
         (#t (list '* m1 m2))))
 
 (define (sum? x)
-  (if (pair? x)
-	    (eq? (car x) '+)
-	    #f))
+  (and (pair? x) (eq? (car x) '+)))
 
 (define (addend s)
   (cadr s))
@@ -41,8 +33,7 @@
   (caddr s))
 
 (define (product? x)
-  (if (pair? x)
-	    (eq? (car x) '*)))
+  (and (pair? x) (eq? (car x) '*)))
 
 (define (multiplier p)
   (cadr p))
